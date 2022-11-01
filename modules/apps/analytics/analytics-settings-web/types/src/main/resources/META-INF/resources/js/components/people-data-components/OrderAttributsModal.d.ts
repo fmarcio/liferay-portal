14924c0ebc85ed3afc0a1aea4358bbcc37327881
Ilza Medeiros <ilza.medeiros@liferay.com>
2022-11-01 17:20:36 -0300

    LRAC-11906 - [F] Generated TS files

--- modules/apps/analytics/analytics-settings-web/types/src/main/resources/META-INF/resources/js/components/people-data-components/OrderAttributsModal.d.ts
+++ modules/apps/analytics/analytics-settings-web/types/src/main/resources/META-INF/resources/js/components/people-data-components/OrderAttributsModal.d.ts
@@ -13,7 +13,9 @@
  */
 
 import React from 'react';
-import {TGenericComponent} from './WizardPage';
-interface IStepProps extends TGenericComponent {}
-declare const Step: React.FC<IStepProps>;
-export default Step;
+interface IOrderAttributesModalProps {
+	observer: any;
+	onCloseModal: () => void;
+}
+declare const OrderAttributesModal: React.FC<IOrderAttributesModalProps>;
+export default OrderAttributesModal;
