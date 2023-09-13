5f3b7ccbb83db59e4f9e8bab44d803066e8f5ea9
Adriano Interaminense <adriano.interaminense@gmail.com>
2023-09-12 23:56:22 -0300

    LRAC-14473 Update unit tests on AC side

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/experiments/hocs/mappers/__tests__/experiment-summary-mapper.js
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/experiments/hocs/mappers/__tests__/experiment-summary-mapper.js
@@ -163,8 +163,6 @@ describe('Summary Mapper for status in RUNNING', () => {
 	});
 
 	it('should return formatted header', () => {
-		expect(mapper.header.modals.length).toBe(1);
-		expect(mapper.header.modals[0].title).toEqual('Terminate Test');
 		expect(mapper.header.Description()).toEqual('Started: Aug 5, 2019');
 		expect(mapper.header.title).toEqual('Test Is Running');
 	});
