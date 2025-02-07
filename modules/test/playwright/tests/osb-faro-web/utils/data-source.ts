00c921065e275dd89baa7b4f8290ad888f63a053
Marcio Fonseca <marcio.fonseca@liferay.com>
2025-02-07 19:36:52 -0300

    LPD-44493 | LPD-48077 create functions for checking, renaming and searching data sources

--- modules/test/playwright/tests/osb-faro-web/utils/data-source.ts
+++ modules/test/playwright/tests/osb-faro-web/utils/data-source.ts
@@ -3,7 +3,27 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import {Page, expect} from '@playwright/test';
+
 import {faroConfig} from '../faro.config';
+
+export async function checkDataSourceStatus(
+	dataSourceName: string,
+	dataSourceStatus: string,
+	page: Page
+) {
+	await expect(
+		page
+			.getByRole('heading', {exact: true, name: dataSourceName})
+			.locator('span')
+	).toBeVisible();
+
+	await expect(
+		page.getByRole('button', {
+			name: `Current Status: ${dataSourceStatus}`,
+		})
+	).toBeVisible();
+}
 
 export async function createDataSource(page) {
 	await page.goto(faroConfig.environment.baseUrl);
@@ -23,4 +43,41 @@ export async function createDataSource(page) {
 	await page.getByLabel('Click to Copy').click();
 
 	await page.getByRole('link', {name: 'Done'}).click();
+}
+
+export async function renameDataSource(
+	newDataSourceName: string,
+	oldDataSourceName: string,
+	page: Page
+) {
+	await page
+		.getByRole('link', {exact: true, name: oldDataSourceName})
+		.click();
+
+	await page.getByRole('button', {name: 'Edit'}).click();
+
+	await page.locator('#dataSourceName').click();
+
+	await page.locator('#dataSourceName').fill(newDataSourceName);
+
+	await page.getByRole('button', {name: 'Submit'}).click();
+
+	await expect(
+		page
+			.getByRole('heading', {exact: true, name: newDataSourceName})
+			.locator('span')
+	).toBeVisible();
+}
+
+export async function searchDataSourceAndClickOnIt(
+	dataSourceName: string,
+	page: Page
+) {
+	await page.getByRole('textbox', {name: 'Search'}).click();
+
+	await page.getByRole('textbox', {name: 'Search'}).fill(dataSourceName);
+
+	await page.getByRole('textbox', {name: 'Search'}).press('Enter');
+
+	await page.getByRole('link', {exact: true, name: dataSourceName}).click();
 }
