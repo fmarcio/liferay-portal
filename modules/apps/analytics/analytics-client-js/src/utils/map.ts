aba6f1a0a3278f55183da6861f99cb6d9e08cfe2
Marcio Fonseca <marcio.fonseca@liferay.com>
2025-02-26 15:54:44 -0300

    LPD-48880 | LPD-49211 convert map.js to typescript

--- modules/apps/analytics/analytics-client-js/src/utils/map.ts
+++ modules/apps/analytics/analytics-client-js/src/utils/map.ts
@@ -3,16 +3,16 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-function convertMapToArr(mapInstance) {
-	const array = [];
+function convertMapToArr(mapInstance: Map<any, any>): Array<any> {
+	const array: any = [];
 
 	mapInstance.forEach((value, key) => array.push([key, value]));
 
 	return array;
 }
 
-function getMapKeys(mapInstance) {
-	const array = [];
+function getMapKeys(mapInstance: Map<any, any>) {
+	const array: any = [];
 
 	mapInstance.forEach((value, key) => array.push(key));
 
