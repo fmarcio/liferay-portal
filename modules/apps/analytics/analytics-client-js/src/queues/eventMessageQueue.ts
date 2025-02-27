838d255522007caee29f25ccb137c0da4c6d40e4
Adriano Interaminense <adriano.interaminense@gmail.com>
2025-02-27 16:11:23 -0300

    LPD-49213 Convert eventMessaQueue to typescript

--- modules/apps/analytics/analytics-client-js/src/queues/eventMessageQueue.ts
+++ modules/apps/analytics/analytics-client-js/src/queues/eventMessageQueue.ts
@@ -3,11 +3,18 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import Analytics from '../analytics';
 import {STORAGE_KEY_MESSAGES} from '../utils/constants';
 import BaseSendMessageQueue from './baseSendMessageQueue';
 
 class EventMessageQueue extends BaseSendMessageQueue {
-	constructor({analyticsInstance, name = STORAGE_KEY_MESSAGES}) {
+	constructor({
+		analyticsInstance,
+		name = STORAGE_KEY_MESSAGES,
+	}: {
+		analyticsInstance: Analytics;
+		name?: string;
+	}) {
 		super({
 			analyticsInstance,
 			flushTo: analyticsInstance.config.endpointUrl,
