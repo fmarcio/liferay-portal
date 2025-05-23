6d56468dd1f1c8ac6b8403332ce441a50edeb369
Marcio Fonseca <marcio.fonseca@liferay.com>
2025-05-23 15:22:12 -0300

    LPD-52301 | LPD-53341 SF

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/ViewDashboardDisplayContext.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/ViewDashboardDisplayContext.java
@@ -5,12 +5,12 @@
 
 package com.liferay.site.cms.site.initializer.internal.display.context;
 
+import com.liferay.object.constants.ObjectFolderConstants;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.PortalUtil;
-import com.liferay.object.constants.ObjectFolderConstants;
 
 import java.util.Map;
 
@@ -19,28 +19,30 @@ import java.util.Map;
  */
 public class ViewDashboardDisplayContext {
 
-	public Map<String, Object> getConstants() {
-	return HashMapBuilder.<String, Object>put(
-		"ercContentStructures",
-		ObjectFolderConstants.EXTERNAL_REFERENCE_CODE_CONTENT_STRUCTURES
-	).put(
-		"ercFileTypes",
-		ObjectFolderConstants.EXTERNAL_REFERENCE_CODE_FILE_TYPES
-	).build();
-}
-
 	public ViewDashboardDisplayContext(ThemeDisplay themeDisplay) {
 		_themeDisplay = themeDisplay;
 	}
 
+	public Map<String, Object> getConstants() {
+		return HashMapBuilder.<String, Object>put(
+			"ercContentStructures",
+			ObjectFolderConstants.EXTERNAL_REFERENCE_CODE_CONTENT_STRUCTURES
+		).put(
+			"ercFileTypes",
+			ObjectFolderConstants.EXTERNAL_REFERENCE_CODE_FILE_TYPES
+		).build();
+	}
+
 	public Map<String, Object> getReactData() throws PortalException {
 		return HashMapBuilder.<String, Object>put(
+			"constants", getConstants()
+		).put(
 			"dashboard",
 			PortalUtil.getLayoutFullURL(
 				LayoutLocalServiceUtil.getLayoutByFriendlyURL(
 					_themeDisplay.getScopeGroupId(), false, "/dashboard"),
 				_themeDisplay)
-		).put("constants", getConstants()).build();
+		).build();
 	}
 
 	private final ThemeDisplay _themeDisplay;
